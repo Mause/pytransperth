85a26dc6e6f04b3049b77e2e1cac57651629a811
Dominic May <jack.thatch@gmail.com>
2014-02-16 16:12:14 +0800

    Test data for the transperth.routes module

---
+++ tests/constants/routes.py
@@ -0,0 +1,10 @@
+from transperth.location import LocationT
+
+ROUTES = {
+    'to': [
+        LocationT('LOCATION1', 'LOCATION1~~;')
+    ],
+    'from': [
+        LocationT('LOCATION2', 'LOCATION2~~;')
+    ]
+}
