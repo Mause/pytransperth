5902c61d3de8db2e31bd78274cee9e97da00e0a4
Dominic May <jack.thatch@gmail.com>
2014-02-17 19:06:33 +0800

    Added a simplistic sample app

---
+++ sample_app/main.py
@@ -0,0 +1,51 @@
+# standard library
+import logging
+import os
+import sys
+
+here = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(here, '..'))
+
+# third party
+import tornado.ioloop
+import tornado.log
+import tornado.web
+
+# application specific
+import transperth.fares
+from utils import fares_to_table, BaseRequestHandler
+
+logging.basicConfig(level=logging.DEBUG)
+tornado.log.enable_pretty_logging()
+
+
+class FaresRequestHandler(BaseRequestHandler):
+    def get(self):
+        self.render('fares.html')
+
+    def post(self):
+        from_loco = self.get_location('from')
+        to_loco = self.get_location('to')
+
+        fares = transperth.fares.determine_fare(
+            from_loco,
+            to_loco
+        )
+
+        table = fares_to_table(fares)
+        self.write(table._repr_html_())
+
+
+settings = {
+    'debug': True,
+    'template_path': os.path.join(here, 'templates')
+}
+
+
+application = tornado.web.Application([
+    (r"/", FaresRequestHandler),
+], **settings)
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
---
+++ sample_app/utils.py
@@ -0,0 +1,40 @@
+
+import tornado.web
+import ipy_table
+from transperth.location import Location
+
+
+class BaseRequestHandler(tornado.web.RequestHandler):
+    @property
+    def args(self):
+        args = self.request.arguments
+
+        return {
+            k: [sv.decode() for sv in v]
+            for k, v in args.items()
+        }
+
+    def get_location(self, key):
+        return Location.from_location(
+            self.get_argument(key)
+        )
+
+
+def fares_to_table(fares):
+    keys, values = zip(*fares.items())
+
+    table_rows = [['Fare Type']]
+    table_rows[-1].extend(key.title() for key in keys)
+
+    for key in values[0].keys():
+        table_rows.append([key.title()])
+
+        table_rows[-1].extend(
+            ticket_type[key]
+            for ticket_type in values
+        )
+
+    table = ipy_table.make_table(table_rows)
+    table.apply_theme('basic')
+
+    return table
