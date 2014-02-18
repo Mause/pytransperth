b4b9e260f9203272e874c99612c72fac6ee32f6f
Dominic May <jack.thatch@gmail.com>
2014-02-18 12:09:18 +0800

    Plumbed in location caching proxy function

--- sample_app/main.py
+++ sample_app/main.py
@@ -13,6 +13,7 @@ import tornado.web
 
 # application specific
 import transperth.fares
+from location_proxy import determine_location
 from utils import fares_to_table, BaseRequestHandler
 
 logging.basicConfig(level=logging.DEBUG)
@@ -26,6 +27,11 @@ class FaresRequestHandler(BaseRequestHandler):
     def post(self):
         from_loco = self.get_location('from')
         to_loco = self.get_location('to')
+
+        locations = determine_location(from_loco, to_loco)
+
+        from_loco = locations['from'][0]
+        to_loco = locations['to'][0]
 
         fares = transperth.fares.determine_fare(
             from_loco,
