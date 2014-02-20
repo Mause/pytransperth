1bc8c05c6495e21392cc66e923f6cdaf1cb66acd
Dominic May <jack.thatch@gmail.com>
2014-02-20 13:07:47 +0800

    Use the read the docs theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,8 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
+import os
+import sys
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -103,7 +105,16 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+# html_theme = 'default'
+
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed
+# from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -268,8 +279,6 @@ texinfo_documents = [
 #texinfo_no_detailmenu = False
 
 
-import os
-import sys
 PATH = os.path.dirname(__file__)
 PATH = os.path.join(PATH, '..', '..')
 PATH = os.path.abspath(PATH)
