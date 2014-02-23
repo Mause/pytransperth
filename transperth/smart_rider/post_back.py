78378a7f0ff0cf10699ae30e947c3e5187d2acc8
Dominic May <jack.thatch@gmail.com>
2014-02-23 18:24:55 +0800

    A somewhat operationable smart rider module implementation
    Some of what is currently hard coded will need to still be made user configurable

---
+++ transperth/smart_rider/post_back.py
@@ -0,0 +1,266 @@
+"""
+The code in this file was ported from javascript from the transperth website.
+
+As I progress, I will attempt to whittle it down to purely what is absolutely
+required.
+"""
+
+from collections import defaultdict
+from pprint import pprint
+
+# from .. import BASE
+BASE = 'https://www.transperth.wa.gov.au/'
+
+_async_post_back_control_client_IDs = set()
+_post_back_control_client_IDs = set()
+_update_panel_client_IDs = [
+    "dnn_ctr2061_SmartRiderTransactions_pnlModuleMessagePanel",
+    "dnn_ctr2061_SmartRiderTransactions_pnlSmartRiderBalancePanel",
+    "dnn_ctr2061_SmartRiderTransactions_rgTransactionsPanel",
+    "dnn_ctr2061_SmartRiderTransactions_rdFromDatePanel",
+    "dnn_ctr2061_SmartRiderTransactions_rdToDatePanel",
+    "RadAjaxManager1SU"
+]
+_update_panel_has_children_as_triggers = [True, True, True, True, True, True]
+_update_panel_IDs = [
+    "dnn$ctr2061$SmartRiderTransactions$pnlModuleMessagePanel",
+    "dnn$ctr2061$SmartRiderTransactions$pnlSmartRiderBalancePanel",
+    "dnn$ctr2061$SmartRiderTransactions$rgTransactionsPanel",
+    "dnn$ctr2061$SmartRiderTransactions$rdFromDatePanel",
+    "dnn$ctr2061$SmartRiderTransactions$rdToDatePanel",
+    "RadAjaxManager1SU"
+]
+_create_post_back_settings = lambda c, b, a: {
+    'async': c,
+    'panelID': b,
+    'sourceElement': a
+}
+
+
+_unique_ID_to_client_ID = lambda a: '_'.join(a.split('$'))
+
+
+def _matches_parent_ID_in_list(c, b):
+    for a in b:
+        if (c.starts_with(a + "_")):
+            return True
+    return False
+
+
+def _find_nearest_element(document, a):
+    while a:
+        d = _unique_ID_to_client_ID(a)
+        c = document.get_element_by_id(d, None)
+
+        if c is not None:
+            return c
+
+        if '$' not in a:
+            return None
+
+        a = '$'.join(
+            a.split('$')[:-1]
+        )
+
+    return None
+
+
+def _get_post_back_settings(document, a, c):
+    d = a
+    b = None
+    while a is not None:
+        if a.attrib['id']:
+            if not b and a.attrib['id'] in _async_post_back_control_client_IDs:
+                b = _create_post_back_settings(
+                    True,
+                    'ScriptManager|' + c,
+                    d
+                )
+            elif not b and a.attrib['id'] in _post_back_control_client_IDs:
+                return _create_post_back_settings(False, None, None)
+            else:
+                if a.attrib['id'] in _update_panel_client_IDs:
+                    e = _update_panel_client_IDs.index(a.attrib['id'])
+                else:
+                    e = -1
+
+                if e != -1:
+                    if (_update_panel_has_children_as_triggers[e]):
+                        return _create_post_back_settings(
+                            True,
+                            _update_panel_IDs[e] + "|" + c,
+                            d
+                        )
+                    else:
+                        return _create_post_back_settings(
+                            True,
+                            'ScriptManager|' + c,
+                            d
+                        )
+
+            if not b:
+                if _matches_parent_ID_in_list(
+                        a.attrib['id'],
+                        _async_post_back_control_client_IDs):
+                    b = _create_post_back_settings(
+                        True,
+                        'ScriptManager|' + c,
+                        d
+                    )
+                elif _matches_parent_ID_in_list(a.attrib['id'],
+                                                _post_back_control_client_IDs):
+                    return _create_post_back_settings(False, None, None)
+
+        a = a.getparent()
+
+    if (not b):
+        return _create_post_back_settings(False, None, None)
+    else:
+        return b
+
+
+def retrieve_postback_settings(document):
+    a = (
+        "dnn$"
+        "ctr2061$"
+        "SmartRiderTransactions$"
+        "rgTransactions$"
+        "ctl00$"
+        "ctl02$"
+        "ctl00$"
+        "ctl00"
+    )
+    print(a)
+
+    f = _unique_ID_to_client_ID(a)
+    d = document.get_element_by_id(f, None)
+
+    if d is None:
+        c = _find_nearest_element(document, a)
+        if c is not None:
+            return _get_post_back_settings(document, c, a)
+        else:
+            return _create_post_back_settings(
+                False, None, None
+            )
+    else:
+        return _get_post_back_settings(document, d, a)
+
+
+def params_from_form(form):
+    params = {}
+    for d in form.xpath('.//input'):
+        f = d.name
+        if not f or f == "ScriptManager":
+            continue
+
+        m = d.tag.upper()
+
+        if m == "INPUT":
+            k = d.type
+            if (k in {"text", "password", "hidden"} or
+                    k in {"checkbox", "radio"} and d.checked):
+                params[f] = d.value if d.value is not None else ''
+
+        elif m == "SELECT":
+            raise NotImplementedError()
+            # u = d.options.length
+            # for (o = 0; o < u; o++):
+            #     q = d.options[o]
+            #     if (q.selected):
+            #         params[f] = q.value
+            #     }
+            # }
+        elif (m == "TEXTAREA"):
+            params[f] = d.value
+        else:
+            raise Exception(m)
+
+    return params
+
+
+def post_back(session, document, form, code, date_from, date_to):
+    params = params_from_form(form)
+
+    _postBackSettings = retrieve_postback_settings(document)
+    params["ScriptManager"] = _postBackSettings['panelID']
+
+    # add in date range
+    params.update({
+        'dnn$ctr2061$SmartRiderTransactions$rdFromDate': (
+            date_from.strftime('%Y-%d-%m')
+        ),
+        'dnn$ctr2061$SmartRiderTransactions$rdFromDate$dateInput': (
+            date_from.strftime('%Y-%d-%m-00-00-00')
+        ),
+        'dnn$ctr2061$SmartRiderTransactions$rdToDate': (
+            date_to.strftime('%Y-%d-%m')
+        ),
+        'dnn$ctr2061$SmartRiderTransactions$rdToDate$dateInput': (
+            date_to.strftime('%Y-%d-%m-00-00-00')
+        )
+    })
+
+    # add in smart rider card selection, for those special few with
+    # more than one
+    params['dnn$ctr2061$SmartRiderTransactions$ddlSmartCardNumber'] = code
+
+    params.update({
+        '__EVENTARGUMENT': (
+            'FireCommand:'
+            'dnn$ctr2061$SmartRiderTransactions$rgTransactions$ctl00;'
+            'PageSize;'
+            '50'
+        )
+    })
+    # 'dnn$ctr2061$SmartRiderTransactions$rgTransactions$ctl00$ctl03$ctl01'
+    # '$PageSizeComboBox': 50,
+    # 'dnn_ctr2061_SmartRiderTransactions_rgTransactions_ctl00_ctl03_ctl01_'
+    # 'PageSizeComboBox_ClientState': '',
+    # 'dnn_ctr2061_SmartRiderTransactions_rgTransactions_ctl00_ctl03_ctl01_'
+    # 'PageSizeComboBox_ClientState': (
+    #     '{"logEntries": [],"value": "50","text": "50","enabled": true}'
+    # )
+
+    pprint(params)
+
+    r = session.post(
+        BASE + "TravelEasy/MySmartRider/tabid/71/Default.aspx",
+        data=params,
+        headers={
+            "X-MicrosoftAjax": "Delta=true",
+            "Cache-Control": "no-cache",
+            "User-Agent": (
+                "Mozilla/5.0 (Windows NT 6.1; WOW64) "
+                "AppleWebKit/537.36 (KHTML, like Gecko) "
+                "Chrome/32.0.1700.107 Safari/537.36)"
+            )
+        }
+    )
+
+    updates = _parse_delta(r.text)
+
+    if updates['pageRedirect']:
+        raise Exception(('Not logged in', updates['pageRedirect']))
+
+    return updates
+
+
+def _parse_delta(c):
+    updates = defaultdict(list)
+    c = c.split('|')
+
+    while c:
+        if c[0] == '':
+            break
+
+        length, frag_type, frag_id, content = (
+            c.pop(0), c.pop(0), c.pop(0), c.pop(0)
+        )
+
+        updates[frag_type].append({
+            'id': frag_id,
+            'content': content
+        })
+
+    return updates
--- transperth/smart_rider/smart_rider.py
+++ transperth/smart_rider/smart_rider.py
@@ -1,19 +1,90 @@
 #!/usr/bin/venv python
-import requests
-from lxml import html, etree
-from itertools import chain
-import json
+"""
+Based off the code that blha303 (http://github/blha303) provided me with
+"""
 
+from dateutil.parser import parse as date_parse
+from itertools import chain
+from pprint import pprint
+import json
+import logging
+import re
+
+from lxml import html, etree
+import requests
+
+from post_back import post_back
+# from .. import BASE
 BASE = 'https://www.transperth.wa.gov.au/'
+
 
 # we actually need a class for the login part of the website,
 # as the session has to be stored somewhere
 
 
-class Transperth(object):
-    """
+logging.basicConfig(level=logging.DEBUG)
 
-    """
+
+class TransperthSession(object):
+    SR_NAME_RE = re.compile(r'(?:\W+([A-Za-z\W]+)\W+)?(\d+)(?:\W+)?')
+
+    def __init__(self, session):
+        self.session = session
+        self._smart_riders = None
+        self._smart_rider_document = None
+        self._smart_rider_form = None
+
+    def smart_riders(self):
+        if not self._smart_riders:
+            select = self.smart_rider_document().get_element_by_id(
+                'dnn_ctr2061_SmartRiderTransactions_ddlSmartCardNumber'
+            )
+
+            self._smart_riders = {}
+            for option in select:
+                name, number = self.SR_NAME_RE.search(option.text).groups()
+
+                self._smart_riders[number] = {
+                    'code': option.get('value'),
+                    'name': name
+                }
+
+        return self._smart_riders
+
+    def smart_rider_document(self):
+        if self._smart_rider_document is None:
+            r = self.session.get(
+                BASE + "TravelEasy/MySmartRider/tabid/71/Default.aspx"
+            )
+            self._smart_rider_document = html.document_fromstring(r.text)
+
+        return self._smart_rider_document
+
+    def smart_rider_form(self):
+        if self._smart_rider_form is None:
+            self._smart_rider_form = self.smart_rider_document().forms[0]
+
+        return self._smart_rider_form
+
+    def get_activities(self, sr_code):
+        date_from, date_to = date_parse('01/01/2010'), date_parse('01/01/2015')
+
+        updates = post_back(
+            self.session,
+            self.smart_rider_document(),
+            self.smart_rider_form(),
+            sr_code,
+            date_from,
+            date_to
+        )
+
+        assert self.session
+        assert self.smart_rider_document is not None
+        assert self.smart_rider_form is not None
+
+        return _get_smart_rider(
+            updates['updatePanel'][1]['content']
+        )
 
 
 def login(username, password):
@@ -51,347 +122,72 @@ def login(username, password):
 
     assert r.url.endswith('Home.aspx'), 'Login failed'
 
-    return s
-
-
-def get_smart_rider(s):
-    r = s.get(BASE + "TravelEasy/MySmartRider/tabid/71/Default.aspx")
-
-    assert r.ok, r.status_code
-    assert r.headers['Content-Type'].startswith('text/html'), (
-        r.headers['Content-Type']
-    )
-
-    return _request_from_form(
-        r.text
-    )
-
-
-def _request_from_form(document):
-    with open('document.html', 'w') as fh:
-        fh.write(document)
-
-    document = html.document_fromstring(document)
-
-    from pprint import pprint
-    pprint(document.forms[0])
-
-    form = document.forms[0]
-
-    print(form, form.attrib)
-
-    # import IPython
-    # IPython.embed()
-
-    _doPostBack(s, document, form)
+    return TransperthSession(s)
 
 
 def _get_smart_rider(root):
-    # assert not root.startswith('132')
-
-    print(root)
-
-    with open('test.html', 'w') as fh:
-        fh.write(root)
-
     root = html.document_fromstring(root)
 
-    table = root.xpath("//table[@class='rgMasterTable']")
-    assert table, table
-
-    table = table[0]
-    print(table)
+    table = root.xpath("//table[@class='rgMasterTable']")[0]
 
     rows = table.find('tbody').findall('tr')
-    print(rows)
     items = (
         map(etree._Element.itertext, row.findall('td'))
         for row in rows
     )
     items = map(chain.from_iterable, items)
-    items = list(map(list, items))
+    items = map(list, items)
 
-    from pprint import pprint
-    pprint(items)
-
-    data = items[0]
-
-    out = {
-        'time': data[0] + " +0800",
-        'action': data[1],
-        'location': (data[2].replace(" a ", " after ")
-                     .replace(" b ", " before")),
-        'service': data[3],
-        'zone': data[4],
-        'cost': data[5],
-        'balance': data[6],
-        'notes': data[7]
-    }
-
-    from pprint import pprint
-    pprint(out)
-
-    # url = BASE + "DesktopModules/JourneyPlanner/JP.aspx".format(out
-        # ["location"])
-
-    # data = requests.get(
-    #     ?from=Location&to=Location&fromStreet=&fromSuburb=&fromLocation={}&f
-    # romStop=&toStreet=&toSuburb=&toLocation=a&toStop=&jpDate=Tuesday,%2011%2
-    # 0February%202014&jpDirection=A&jpAMPM=PM&jpHour=8&jpMinute=30&fSet=False
-    # &fGadget=False&mode=t1,b1,f1,s0&jpnMaxJourneys=5&jpMaxChanges=-1&jpWalkCh
-    # ange=NORMAL&jpWheelchairOnly=0
-    # )
-
-    # data = Soup(requests.get(url).text)
-
-
-    # if " after " in out["location"] or " before " in out["location"]: # Bus
-    #     term = out["location"].split(" ")[-1]
-    #     for a in data.findall('fromlocation'):
-    #         text = a.find('display').text
-    #         if not term in text:
-    #             continue
-    #         gterm = text.split(" (")[0]
-    #     if not gterm:
-    #         gterm = "Perth"
-    # else: # probably train
-    #     gterm = out["location"]
-    # out["gterm"] = gterm
-    # out["text"] = "Steven was last seen doing a {action} at {location}. He
-    # has {balance} left on his SmartRider.".format(**out)
-    # with open('/home/blha/sites/blha303.com.au/where/transperthinfo.json'
-    #     'w') as f:
-    #     f.write(json.dumps(out))
-
-
-_uniqueIDToClientID = lambda a: '_'.join(a.split('$'))
-
-
-def _findNearestElement(document, a):
-    while a:
-        d = _uniqueIDToClientID(a)
-        c = document.get_element_by_id(d, None)
-
-        if c is not None:
-            return c
-
-        if '$' not in a:
-            return None
-
-        a = '$'.join(
-            a.split('$')[:-1]
+    actions = []
+    for action in items:
+        action[2] = (
+            action[2]
+            .replace(" a ", " after ")
+            .replace(" b ", " before")
+            .replace('nnn', 'annin')
         )
 
-    return None
+        actions.append({
+            'time': date_parse(action[0] + " +0800"),
+            'action': action[1],
+            'location': action[2],
+            'service': action[3],
+            'zone': action[4],
+            'amount': 0 if action[5] == '\xa0' else action[5],
+            'balance': float(action[6]),
+            'notes': action[7]
+        })
 
-_asyncPostBackControlClientIDs = set()
-_postBackControlClientIDs = set()
-_updatePanelClientIDs = [
-    "dnn_ctr2061_SmartRiderTransactions_pnlModuleMessagePanel",
-    "dnn_ctr2061_SmartRiderTransactions_pnlSmartRiderBalancePanel",
-    "dnn_ctr2061_SmartRiderTransactions_rgTransactionsPanel",
-    "dnn_ctr2061_SmartRiderTransactions_rdFromDatePanel",
-    "dnn_ctr2061_SmartRiderTransactions_rdToDatePanel",
-    "RadAjaxManager1SU"
-]
-_scriptManagerID = 'arbitrary'
-_updatePanelHasChildrenAsTriggers = [True, True, True, True, True, True]
-_updatePanelIDs = [
-    "dnn$ctr2061$SmartRiderTransactions$pnlModuleMessagePanel",
-    "dnn$ctr2061$SmartRiderTransactions$pnlSmartRiderBalancePanel",
-    "dnn$ctr2061$SmartRiderTransactions$rgTransactionsPanel",
-    "dnn$ctr2061$SmartRiderTransactions$rdFromDatePanel",
-    "dnn$ctr2061$SmartRiderTransactions$rdToDatePanel",
-    "RadAjaxManager1SU"
-]
+    pagination = root.xpath("//div[@class='rgWrap rgInfoPart']")[0]
+    items = int(list(pagination.itertext())[1])
 
-
-_createPostBackSettings = lambda c, b, a: {
-    'async': c,
-    'panelID': b,
-    'sourceElement': a
-}
-
-_matchesParentIDInList = lambda a, b: a in b
-
-
-def _getPostBackSettings(document, a, c):
-    d = a
-    b = None
-    while a is not None:
-        if a.attrib['id']:
-            if not b and a.attrib['id'] in _asyncPostBackControlClientIDs:
-                b = _createPostBackSettings(
-                    True,
-                    _scriptManagerID + "|" + c,
-                    d
-                )
-            elif not b and a.attrib['id'] in _postBackControlClientIDs:
-                return _createPostBackSettings(False, None, None)
-            else:
-                if a.attrib['id'] in _updatePanelClientIDs:
-                    e = _updatePanelClientIDs.index(a.attrib['id'])
-                else:
-                    e = -1
-
-                if e != -1:
-                    if (_updatePanelHasChildrenAsTriggers[e]):
-                        return _createPostBackSettings(
-                            True,
-                            _updatePanelIDs[e] + "|" + c,
-                            d
-                        )
-                    else:
-                        return _createPostBackSettings(
-                            True,
-                            _scriptManagerID + "|" + c,
-                            d
-                        )
-
-            if not b:
-                if _matchesParentIDInList(a.attrib['id'],
-                                          _asyncPostBackControlClientIDs):
-                    b = _createPostBackSettings(
-                        True,
-                        _scriptManagerID + "|" + c,
-                        d
-                    )
-                elif _matchesParentIDInList(a.attrib['id'], _postBackControlClientIDs):
-                    return _createPostBackSettings(False, None, None)
-
-        a = a.getparent()
-
-    if (not b):
-        return _createPostBackSettings(False, None, None)
-    else:
-        return b
-
-
-def _doPostBack(session, document, form):
-    a = (
-        "dnn$"
-        "ctr2061$"
-        "SmartRiderTransactions$"
-        "rgTransactions$"
-        "ctl00$"
-        "ctl02$"
-        "ctl00$"
-        "ctl00"
-    )
-    e = ""
-
-    f = _uniqueIDToClientID(a)
-    d = document.get_element_by_id(f, None)
-
-    if not d:
-        c = _findNearestElement(document, a)
-        if c is not None:
-            _postBackSettings = _getPostBackSettings(document, c, a)
-        else:
-            _postBackSettings = _createPostBackSettings(
-                False, None, None
-            )
-    else:
-        _postBackSettings = _getPostBackSettings(document, d, a)
-
-    from pprint import pprint
-    pprint(_postBackSettings)
-
-    # b = object()
-    # b.__EVENTTARGET.value = a
-    # b.__EVENTARGUMENT.value = e
-    _onFormSubmit(session, form, _postBackSettings)
-
-
-def _onFormSubmit(session, form, _postBackSettings):
-    params = {}
-    params["ScriptManager"] = _postBackSettings['panelID']
-
-    for d in form.xpath('.//input'):
-        f = d.name
-        if not f or f == "ScriptManager":
-            continue
-
-        # m = d.tagName.toUpperCase()
-        m = d.tag.upper()
-
-        if m == "INPUT":
-            k = d.type
-            if (k in {"text", "password", "hidden"} or
-                    k in {"checkbox", "radio"} and d.checked):
-                params[f] = d.value
-
-        elif m == "SELECT":
-            raise NotImplementedError()
-            # u = d.options.length
-            # for (o = 0; o < u; o++) {
-            #     q = d.options[o]
-            #     if (q.selected) {
-            #         params[f] = q.value
-            #     }
-            # }
-        elif (m == "TEXTAREA"):
-            params[f] = d.value
-        else:
-            raise Exception(m)
-
-    # params['__ASYNCPOST'] = 'true'
-
-    r = session.post(
-        BASE + "TravelEasy/MySmartRider/tabid/71/Default.aspx",
-        data=params,
-        headers={
-            "X-MicrosoftAjax": "Delta=true",
-            "Cache-Control": "no-cache",
-            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.107 Safari/537.36"
-        }
-    )
-
-    with open('afterall.html', 'w') as fh:
-        fh.write(r.text)
-
-    print(r)
-
-    import IPython
-    IPython.embed()
-
-    # i, j = this._get_eventHandlerList().getHandler("initializeRequest")
-    # if j:
-    #     i = Sys.WebForms.InitializeRequestEventArgs(
-    #         c, _postBackSettings.sourceElement
-    #     )
-    #     j(this, i)
-    #     g = not i.get_cancel()
-
-    # this.abortPostBack()
-    # j = this._get_eventHandlerList().getHandler("beginRequest")
-    # if j:
-    #     i = Sys.WebForms.BeginRequestEventArgs(
-    #         c, _postBackSettings.sourceElement)
-    #     j(this, i)
-    # if (this._originalDoCallback):
-    #     this._cancelPendingCallbacks()
-    # this._request = c
-    # this._processingRequest = False
-    # c.invoke()
-    # if (h):
-    #     h.preventDefault()
-
-
+    return {
+        'actions': actions,
+        'actions_total': items
+    }
 
 if __name__ == '__main__':
     import os
 
     if not os.path.exists('session.json'):
-        print('loggging in...')
+        print('logging in...')
 
         s = login(*open('auth').read().split(','))
 
+        print('login succesful')
+
         with open('session.json', 'w') as fh:
-            json.dump(s.cookies.get_dict(), fh, indent=4)
+            json.dump(s.session.cookies.get_dict(), fh, indent=4)
     else:
         s = requests.Session()
         with open('session.json') as fh:
             s.cookies.update(json.load(fh))
+        s = TransperthSession(s)
 
-    get_smart_rider(s)
+    riders = s.smart_riders()
+
+    pprint(riders)
+
+    act = s.get_activities('222664')
+
+    pprint(act)
