5e595878abf8b9cd2b3ce080713150b45265105b
Dominic May <jack.thatch@gmail.com>
2014-03-13 13:06:18 +0800

    Mock out lxml when building docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -318,3 +318,31 @@ class MethodDocumenter(BetterSigMixin, autodoc.MethodDocumenter):
 def setup(app):
     autodoc.add_documenter(FunctionDocumenter)
     autodoc.add_documenter(MethodDocumenter)
+
+import sys
+
+
+class Mock(object):
+
+    __all__ = []
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['lxml', 'lxml.html']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
