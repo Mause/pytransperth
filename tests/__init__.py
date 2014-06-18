d45a35aa2306e74c455f80681f2472462a93a658
Dominic May <jack.thatch@gmail.com>
2014-06-18 18:30:21 +0800

    Rework test discovery

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,7 +3,8 @@ from os.path import join, dirname, splitext, abspath, relpath
 import sys
 import unittest
 
-MODULE_DIR = join(dirname(__file__), '..')
+HERE = dirname(__file__)
+MODULE_DIR = join(HERE, '..')
 MODULE_DIR = abspath(MODULE_DIR)
 
 is_test = lambda filename: (
@@ -21,17 +22,15 @@ def walker(opath='.'):
                 rpath = relpath(path, opath)
 
                 # build up the import path
-                yield (rpath + '.' + splitext(filename)[0]).strip('.')
+                yield 'tests.{}.{}'.format(rpath, splitext(filename)[0])
 
 
 def suite():
-    sys.path.insert(0, MODULE_DIR)
-    sys.path.insert(0, dirname(__file__))
+    sys.path = [HERE, MODULE_DIR] + sys.path
 
-    SUB_UNITS = dirname(__file__)
-    SUB_UNITS = walker(SUB_UNITS)
+    SUB_UNITS = walker(HERE)
 
-    chdir(dirname(__file__))
+    chdir(HERE)
 
     return unittest.TestLoader().loadTestsFromNames(SUB_UNITS)
 
