2e5dad8e069fd98935f7b03855a791a73e04f7c7
Dominic May <jack.thatch@gmail.com>
2014-06-18 18:23:34 +0800

    Timetable module for bus stops

---
+++ transperth/silver_rails/timetable.py
@@ -0,0 +1,141 @@
+"""
+Contributed by blha303
+
+API key can be retrieved from the android app with Root Browser
+(/data/data/au.gov.wa.pta.transperth/com.jeppesen.transperth.xml)
+Please don't abuse this service, you'll ruin it for the rest of us.
+Use reasonable limits on your code (i'm using time.sleep(5))
+There's no current list of transperth stop numbers (i'm still looking), so try
+iterating over xrange(10000, 100000) (with reasonable limits as detailed above)
+Produces a dict of stop numbers, with bus numbers and stop times.
+{'10000': [
+           [["220", "05:22"], ["220": "06:07"]], # weekday
+           [], # saturday
+           [] # sunday
+          ],
+ '10001': [[], [], []] }
+Script is currently set to 2014-06-17 (a weekday), so it'll populate that list
+for each stop number. Change "date =" and out[a][0] to point to a saturday and
+[1], or a sunday and [2].
+"""
+
+import datetime
+import dateutil.parser
+import logging
+import requests
+import xml.etree.ElementTree as ET
+
+from . import BASE
+from .utils import prepare_url
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+# included here because the pypi installer is broken
+class XML2Dict(object):
+    def __init__(self, coding='UTF-8'):
+        self._coding = coding
+
+    def _parse_node(self, node):
+        tree = {}
+        # Save childrens
+        for child in node.getchildren():
+            ctag = child.tag
+            cattr = child.attrib
+            ctext = (
+                child.text.strip()  # .encode(self._coding)
+                if child.text is not None else ''
+            )
+            ctree = self._parse_node(child)
+            if not ctree:
+                cdict = self._make_dict(ctag, ctext, cattr)
+            else:
+                cdict = self._make_dict(ctag, ctree, cattr)
+            if ctag not in tree:  # First time found
+                tree.update(cdict)
+                continue
+            atag = '@' + ctag
+            atree = tree[ctag]
+            if not isinstance(atree, list):
+                if not isinstance(atree, dict):
+                    atree = {}
+                if atag in tree:
+                    atree['#'+ctag] = tree[atag]
+                    del tree[atag]
+                tree[ctag] = [atree]  # Multi entries, change to list
+            if cattr:
+                ctree['#'+ctag] = cattr
+            tree[ctag].append(ctree)
+        return tree
+
+    def _make_dict(self, tag, value, attr=None):
+        ret = {tag: value}
+        # Save attributes as @tag value
+        if attr:
+            atag = '@' + tag
+            aattr = {}
+            for k, v in attr.items():
+                aattr[k] = v
+            ret[atag] = aattr
+            del atag
+            del aattr
+        return ret
+
+    def parse(self, xml):
+        EL = ET.fromstring(xml)
+        return self._make_dict(EL.tag, self._parse_node(EL), EL.attrib)
+
+
+class RateLimitExceeded(Exception):
+    pass
+
+
+def timetable_for_stop(
+        stop_num: int, time: datetime.datetime,
+        apikey: str,
+        dataset='PerthRestricted'):
+
+    url = BASE + "/rest/Datasets/:dataset/StopTimetable"
+    url = prepare_url(url, {'dataset': dataset})
+
+    req = requests.get(
+        url,
+        params={
+            "stop": stop_num,
+            "time": time.strftime('%Y-%m-%dT%H:%M'),
+            "ApiKey": apikey,
+            "timeband": "1440"
+        }
+    )
+
+    xml = req.text
+    xml = xml.replace('xmlns="http://www.jeppesen.com/journeyplanner" ', "")
+
+    xml = XML2Dict().parse(xml)
+    xml = xml["StopTimetableResponse"]
+
+    status = xml['Status']
+    if status["Severity"] != "Success":
+        details = status["Details"]
+
+        from pprint import pprint
+        pprint(details)
+        if details["StatusDetail"]["Code"] == "APIKeyDailyLimitExceeded":
+            raise RateLimitExceeded()
+
+        raise Exception("{} fetching {}".format(
+            xml["Status"]["Severity"],
+            stop_num
+        ))
+
+    return _parse_timetable_response(xml)
+
+
+def _parse_timetable_response(xml):
+    return [
+        [
+            b["Summary"]["RouteCode"],
+            dateutil.parser.parse(b["DepartTime"])
+        ]
+        for b in xml["Trips"]["StopTimetableTrip"]
+    ]
