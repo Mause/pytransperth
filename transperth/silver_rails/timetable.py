411c6a38b8451dc917927bdc4fdb70aeb9acd52b
Dominic May <jack.thatch@gmail.com>
2014-06-19 15:23:43 +0800

    Just use lxml directly, with xpath

--- transperth/silver_rails/timetable.py
+++ transperth/silver_rails/timetable.py
@@ -7,71 +7,16 @@ API key can be retrieved from the android app with Root Browser
 Please don't abuse this service, you'll ruin it for the rest of us.
 """
 
+from lxml.etree import fromstring
 import datetime
 import dateutil.parser
 import logging
 import requests
-import xml.etree.ElementTree as ET
 
 from . import BASE
 from .utils import prepare_url
 
 logging.basicConfig(level=logging.DEBUG)
-
-
-# included here because the pypi installer is broken
-class XML2Dict(object):
-    def __init__(self, coding='UTF-8'):
-        self._coding = coding
-
-    def _parse_node(self, node):
-        tree = {}
-        # Save childrens
-        for child in node.getchildren():
-            ctag = child.tag
-            cattr = child.attrib
-            ctext = (
-                child.text.strip()  # .encode(self._coding)
-                if child.text is not None else ''
-            )
-            ctree = self._parse_node(child)
-            if not ctree:
-                cdict = self._make_dict(ctag, ctext, cattr)
-            else:
-                cdict = self._make_dict(ctag, ctree, cattr)
-            if ctag not in tree:  # First time found
-                tree.update(cdict)
-                continue
-            atag = '@' + ctag
-            atree = tree[ctag]
-            if not isinstance(atree, list):
-                if not isinstance(atree, dict):
-                    atree = {}
-                if atag in tree:
-                    atree['#'+ctag] = tree[atag]
-                    del tree[atag]
-                tree[ctag] = [atree]  # Multi entries, change to list
-            if cattr:
-                ctree['#'+ctag] = cattr
-            tree[ctag].append(ctree)
-        return tree
-
-    def _make_dict(self, tag, value, attr=None):
-        ret = {tag: value}
-        # Save attributes as @tag value
-        if attr:
-            atag = '@' + tag
-            aattr = {}
-            for k, v in attr.items():
-                aattr[k] = v
-            ret[atag] = aattr
-            del atag
-            del aattr
-        return ret
-
-    def parse(self, xml):
-        EL = ET.fromstring(xml)
-        return self._make_dict(EL.tag, self._parse_node(EL), EL.attrib)
 
 
 class RateLimitExceeded(Exception):
@@ -101,21 +46,18 @@ def timetable_for_stop(
 
     xml = req.text
     xml = xml.replace('xmlns="http://www.jeppesen.com/journeyplanner" ', "")
+    xml = fromstring(xml)
 
-    xml = XML2Dict().parse(xml)
-    xml = xml["StopTimetableResponse"]
+    status = xml.xpath('Status')[0]
+    severity = status.xpath("Severity/text()")[0]
+    if severity != "Success":
 
-    status = xml['Status']
-    if status["Severity"] != "Success":
-        details = status["Details"]
-
-        from pprint import pprint
-        pprint(details)
-        if details["StatusDetail"]["Code"] == "APIKeyDailyLimitExceeded":
+        code = status.xpath("Details/StatusDetail/Code/text()")[0]
+        if code == "APIKeyDailyLimitExceeded":
             raise RateLimitExceeded()
 
         raise Exception("{} fetching {}".format(
-            xml["Status"]["Severity"],
+            severity,
             stop_num
         ))
 
@@ -125,8 +67,8 @@ def timetable_for_stop(
 def _parse_timetable_response(xml):
     return [
         (
-            b["Summary"]["RouteCode"],
-            dateutil.parser.parse(b["DepartTime"])
+            b.xpath("Summary/RouteCode/text()")[0],
+            dateutil.parser.parse(b.xpath("DepartTime/text()")[0])
         )
-        for b in xml["Trips"]["StopTimetableTrip"]
+        for b in xml.xpath('Trips/StopTimetableTrip')
     ]
